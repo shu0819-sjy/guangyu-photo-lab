56818ae268d2ef3d7f122f8375a65acc65ca7abd
shu0819-sjy <259036684+shu0819-sjy@users.noreply.github.com>
2026-09-25 17:53:30 +0800

    ci: add eslint+build workflow, MIT LICENSE, rename package

---
+++ eslint.config.js
@@ -0,0 +1,31 @@
+import js from "@eslint/js";
+import globals from "globals";
+
+export default [
+  {
+    ignores: ["dist/**", "node_modules/**"],
+  },
+  {
+    files: ["src/**/*.js", "vite.config.js"],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      "no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+        },
+      ],
+      "no-empty": ["error", { allowEmptyCatch: true }],
+    },
+  },
+];
